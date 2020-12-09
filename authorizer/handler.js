8c0e238791a8e96e8b279b709ef4208e8a5e85cb
Andy March <mail@andymarch.co.uk>
2020-12-09 22:29:37 +0000

    Improvements based on feedback to reduce AWS knowledge requirement
    Made the API accessible by default with a sample AuthResponse
    Added OKTA_CLIENT_SECRET as env
    Disabled caching on sensitive authorizer

--- authorizer/handler.js
+++ authorizer/handler.js
@@ -1,11 +1,37 @@
 exports.auth = function (event,context){
-    //implement me
+    let bearer = event.authorizationToken
+    //TASK 1
+    //implement verification
+    context.succeed(generateAuthResponse("Unknown User", 'Allow',  event.methodArn))
+    //context.fail('Unauthorized')
 }
 
 exports.admin = function (event,context){
-    //implement me
+    let bearer = event.authorizationToken
+    //TASK 2
+    //implement verification with a given scope
+    context.succeed(generateAuthResponse("Unknown User", 'Allow',  event.methodArn))
+    //context.fail('Unauthorized')
 }
 
 exports.sensitive = function (event,context){
-    //implement me
+    let bearer = event.authorizationToken
+    //TASK 3
+    //implement introspection
+    context.succeed(generateAuthResponse("Unknown User", 'Allow',  event.methodArn))
+    //context.fail('Unauthorized')
+}
+
+function generateAuthResponse(principalId, effect, methodArn) {
+    return {
+        'principalId': principalId,
+        'policyDocument': {
+            Version: '2012-10-17',
+            Statement: [{
+                Action: 'execute-api:Invoke',
+                Effect: effect,
+                Resource: methodArn
+            }]
+        }
+    }
 }
